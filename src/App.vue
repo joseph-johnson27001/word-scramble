109e47fc472d1373a099c913a37fb4543cb64fa4
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-09-05 14:49:57 +0700

    Global Button Styles

--- src/App.vue
+++ src/App.vue
@@ -34,10 +34,32 @@ body {
   justify-content: center;
   align-items: center;
   background-color: #ffffff;
-  max-width: 1400px;
+  max-width: 1200px;
   height: 100vh;
   width: 100vw;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
+}
+
+button {
+  margin: 10px;
+  padding: 15px 30px;
+  font-size: 18px;
+  color: #fff;
+  background-color: #ff5722;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+  transition: background-color 0.3s ease, transform 0.2s ease;
+}
+
+button:hover {
+  background-color: #ff784e;
+  transform: scale(1.05);
+}
+
+button:focus {
+  outline: none;
 }
 </style>
--- src/components/GameModal.vue
+++ src/components/GameModal.vue
@@ -20,25 +20,4 @@ export default {
   justify-content: center;
   align-items: center;
 }
-
-button {
-  padding: 15px 30px;
-  font-size: 18px;
-  color: #fff;
-  background-color: #ff5722;
-  border: none;
-  border-radius: 8px;
-  cursor: pointer;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-  transition: background-color 0.3s ease, transform 0.2s ease;
-}
-
-button:hover {
-  background-color: #ff784e;
-  transform: scale(1.05);
-}
-
-button:focus {
-  outline: none;
-}
 </style>
